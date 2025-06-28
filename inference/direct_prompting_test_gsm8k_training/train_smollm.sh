330aa2a6ef9920723a809222974c8caa17a4ed74
fe-bit <patrik.felbinger@web.de>
2025-06-28 15:32:15 +0200

    add gsm8k specific training

---
+++ inference/direct_prompting_test_gsm8k_training/train_smollm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# sbatch ./inference/direct_prompting_test_gsm8k_training/train_smollm.sh
+#SBATCH --job-name=Qwen-ReWOO-Train
+#SBATCH -o ./inference/direct_prompting_test_gsm8k_training/jobs/train/smollm/%x.%j.out
+#SBATCH -e ./inference/direct_prompting_test_gsm8k_training/jobs/train/smollm/%x.%j.err
+#SBATCH -D ./
+#SBATCH --time=10:30:00
+#SBATCH --partition=NvidiaAll
+#SBATCH --comment=""
+
+# Add aggressive memory optimization
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True,max_split_size_mb:128
+export CUDA_LAUNCH_BLOCKING=0
+export PYTHONUNBUFFERED=1
+
+lscpu
+
+source env/bin/activate
+
+python3 ./inference/direct_prompting_test_gsm8k_training/train.py HuggingFaceTB/SmolLM2-135M-Instruct
+python3 inference/direct_prompting_test_gsm8k_training/main.py --model-name HuggingFaceTB/SmolLM2-135M-Instruct --first-n 50
